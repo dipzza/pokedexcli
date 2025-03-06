7cfee092c59e62bbaff41f440dedcf2d76f93350
dipzza <dipzza@disroot.org>
2025-03-06 13:49:40 +0100

    Add base REPL

--- main.go
+++ main.go
@@ -2,8 +2,80 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"bufio"
+	"os"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+type cliCommand struct {
+	name 				string
+	description string
+	callback		func() error
 }
+
+var cliCommands map[string]cliCommand
+
+func init() {
+	cliCommands = map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for true {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+
+		if len(words) > 0 {
+			command, ok := cliCommands[words[0]]
+			if !ok {
+				fmt.Println("Unknown command:", words[0])
+				continue
+			}
+
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println(`
+Welcome to the Pokedex!
+Usage:
+`)
+
+	for key, value := range cliCommands {
+		fmt.Println(key, ":", value.description)
+	}
+
+	return nil
+}
+
+func cleanInput(text string) []string {
+	lowerText := strings.ToLower(text)
+	words := strings.Fields(lowerText)
+	return words
+}
+
---
+++ repl_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "With UpperCase ",
+			expected: []string{"with", "uppercase"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected len %v, got %v", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			if actual[i] != c.expected[i] {
+				t.Errorf("Expected %v, got %v", c.expected, actual)
+			}
+		}
+	}
+}
