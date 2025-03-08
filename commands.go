eb90b101c9fbdf765a381dd1189e5fdb7b8fb867
dipzza <dipzza@disroot.org>
2025-03-08 20:53:56 +0100

    Move commands to their own file

---
+++ commands.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dipzza/pokedexcli/internal/pokeapi"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+type config struct {
+	nextURL string
+	prevURL string
+}
+
+var cliCommands map[string]cliCommand
+
+func init() {
+	cliCommands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 locations in the map",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 locations in the map",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp(*config) error {
+	fmt.Print(
+		"Welcome to the Pokedex!\n",
+		"Usage:\n\n",
+	)
+
+	for key, value := range cliCommands {
+		fmt.Println(key + ":", value.description)
+	}
+
+	return nil
+}
+
+func commandMap(c *config) error {
+	if c.nextURL == "" {
+		fmt.Println("You already are at the end of the map.")
+		return nil
+	}
+
+	locations, err := pokeapi.GetResourcePage(c.nextURL)
+	if err != nil {
+		return err
+	}
+
+	c.nextURL = locations.Next
+	c.prevURL = locations.Previous
+
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.prevURL == "" {
+		fmt.Println("You already are at the beggining of the map.")
+		return nil
+	}
+
+	locations, err := pokeapi.GetResourcePage(c.prevURL)
+	if err != nil {
+		return err
+	}
+
+	c.nextURL = locations.Next
+	c.prevURL = locations.Previous
+
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+
+func commandExit(*config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
--- main.go
+++ main.go
@@ -9,48 +9,10 @@ import (
 	"github.com/dipzza/pokedexcli/internal/pokeapi"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func(*config) error
-}
-
-type config struct {
-	nextURL string
-	prevURL string
-}
-
-var cliCommands map[string]cliCommand
-
-func init() {
-	cliCommands = map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"map": {
-			name:        "map",
-			description: "Displays the next 20 locations in the map",
-			callback:    commandMap,
-		},
-		"mapb": {
-			name:        "mapb",
-			description: "Displays the previous 20 locations in the map",
-			callback:    commandMapb,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	locationsConfig := config{
-		nextURL: pokeapi.LocationEndpoint,
+		nextURL: pokeapi.LocationAreaEndpoint,
 		prevURL: "",
 	}
 
@@ -73,67 +35,6 @@ func main() {
 			}
 		}
 	}
-}
-
-func commandExit(*config) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(*config) error {
-	fmt.Print(
-		"Welcome to the Pokedex!\n",
-		"Usage:\n\n",
-	)
-
-	for key, value := range cliCommands {
-		fmt.Println(key + ":", value.description)
-	}
-
-	return nil
-}
-
-func commandMap(c *config) error {
-	if c.nextURL == "" {
-		fmt.Println("You already are at the end of the map.")
-		return nil
-	}
-
-	locations, err := pokeapi.GetResourcePage(c.nextURL)
-	if err != nil {
-		return err
-	}
-
-	c.nextURL = locations.Next
-	c.prevURL = locations.Previous
-
-	for _, location := range locations.Results {
-		fmt.Println(location.Name)
-	}
-
-	return nil
-}
-
-func commandMapb(c *config) error {
-	if c.prevURL == "" {
-		fmt.Println("You already are at the beggining of the map.")
-		return nil
-	}
-
-	locations, err := pokeapi.GetResourcePage(c.prevURL)
-	if err != nil {
-		return err
-	}
-
-	c.nextURL = locations.Next
-	c.prevURL = locations.Previous
-
-	for _, location := range locations.Results {
-		fmt.Println(location.Name)
-	}
-
-	return nil
 }
 
 func cleanInput(text string) []string {
